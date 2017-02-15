d2b45b7682549909924adf429a06c22375de9f59
hennnnes <hroemmer@googlemail.com>
2017-02-15 21:00:31 +0100

    added routes, basic functions

--- Private/server/server.js
+++ Private/server/server.js
@@ -1,38 +1,90 @@
 var express = require('express');
 var app = express();
-var fs = require("fs");
+//var fs = require("fs");
 
+var bodyParser = require("body-parser");
 var Redoid = require('redoid');
+
+
+// body-parser to get data from post request
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+// init redoid
 var redoid = Redoid({
-    color: '#ff3200'
-});
-
-app.get('/', function (req, res) {
-      res.end( JSON.stringify('startseite'));
-});
-
-
-app.get('/start', function (req, res) {
-      console.log( data );
-      res.end( data );
-   });
-});
-
-app.get('/stop', function (req, res) {
-  redoid.stop();
-  console.log('redoid stopped');
-  res.redirect('/');
-});
-
-
-app.get('/off', function (req, res) {
-  redoid.turnOff([200]);
-  console.log('Dioder turned off');
-  res.redirect('/');
-});
-
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+  color: '#ffffff'
 })
+
+// sample route
+router.get('/', function(req, res) {
+  res.json({ message: 'welcome to my api! '});
+});
+
+
+//  more routes for sample stuff
+app.use('/api', router);
+
+router.route('/color')
+  .post(function(req, res) {
+      var color = req.body.color;
+      if (redoid.isColorValid(color)) {
+        redoid.change(color);
+        res.json({message: 'color changed to: ' + color});
+      } else {
+        res.json({message: 'no valid color'});
+      }
+  })
+  .get(function(req, res) {
+      res.json({color: redoid.getColorHexValue()});
+  })
+
+router.route('/stop')
+  .get(function(req, res) {
+    redoid.turnOff([0]);
+    res.json({message: 'Dioder turned off'});
+  })
+
+
+// START Server
+app.listen(port);
+console.log('Magically Port: ' + port);
+
+
+
+// var redoid = Redoid({
+//     color: '#ff3200'
+// });
+//
+// app.get('/', function (req, res) {
+//       res.end( JSON.stringify('startseite'));
+// });
+//
+//
+// app.get('/start', function (req, res) {
+//       console.log( data );
+//       res.end( data );
+//    });
+// });
+//
+// app.get('/stop', function (req, res) {
+//   redoid.stop();
+//   console.log('redoid stopped');
+//   res.redirect('/');
+// });
+//
+//
+// app.get('/off', function (req, res) {
+//   redoid.turnOff([200]);
+//   console.log('Dioder turned off');
+//   res.redirect('/');
+// });
+//
+// var server = app.listen(8081, function () {
+//    var host = server.address().address
+//    var port = server.address().port
+//    console.log("Example app listening at http://%s:%s", host, port)
+// })

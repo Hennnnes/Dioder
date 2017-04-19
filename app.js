5a0e4865c7a8776eaa10fc663fcd4e9a5c13ac93
hennnnes <hroemmer@googlemail.com>
2017-04-19 20:56:35 +0200

    removed app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-var api = require('./api/api.js');
-var frontend = require('./frontend/frontend.js');
-
-app.use(express.static(__dirname + '/../Public'));
-app.use(api);
-app.use(frontend);
